2082c01ee33b09ec8b287bc9fe5558925cb437c9
Sebastian Wesołowski <Sebastian.wesolowski.sw@gmail.com>
2022-07-18 14:52:05 +0200

    d1

---
+++ src/pages/api/a.ts
@@ -0,0 +1,56 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import NextCors from 'nextjs-cors';
+
+import createMinisymposium from 'services/miniSymposium/createAirtable';
+
+const minisymposium = async (req: NextApiRequest, res: NextApiResponse): Promise<unknown> => {
+  await NextCors(req, res, {
+    methods: ['POST'],
+    origin: '*',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+
+  const { method } = req;
+
+  return new Promise((resolve) => {
+    if (method === 'GET') {
+      return res.status(200).json({ message: 'Method not allowed. - GET - minisymposium' });
+    }
+
+    if (method === 'POST') {
+      try {
+        const payload = req.body;
+        const minisymposiumLead = createMinisymposium(payload);
+        return res.status(200).json({ status: 'created', minisymposiumLead });
+      } catch (error) {
+        return res.status(422).json({ status: 'not_created', error });
+      }
+    }
+
+    console.log('🚀 ~ file: index.ts ~ line 17 ~ returnnewPromise ~ resolve', resolve);
+    res.status(400);
+    return res.status(400).json({ message: 'bad_request' });
+  });
+
+  // switch (req.method) {
+  //   case 'GET': {
+  //     return res.status(200).json({ message: 'Method not allowed.' });
+  //   }
+  //   case 'POST': {
+  //     try {
+  //       const payload = req.body;
+  //       const minisymposiumLead = await createMinisymposium(payload);
+  //       res.status(200).json({ status: 'created', minisymposiumLead });
+  //     } catch (error) {
+  //       res.status(422).json({ status: 'not_created', error });
+  //     }
+  //     break;
+  //   }
+
+  //   default:
+  //     res.status(400);
+  //     return res.status(400).json({ message: 'bad_request' });
+  // }
+};
+
+export default minisymposium;
