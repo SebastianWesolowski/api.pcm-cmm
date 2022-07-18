653fe161e30552c3dacbc4876902b0912c1ed1a3
Sebastian Wesołowski <Sebastian.wesolowski.sw@gmail.com>
2022-07-18 14:56:45 +0200

    d2

--- src/pages/api/a.ts
+++ src/pages/api/a.ts
@@ -1,8 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import NextCors from 'nextjs-cors';
 
-import createMinisymposium from 'services/miniSymposium/createAirtable';
-
 const minisymposium = async (req: NextApiRequest, res: NextApiResponse): Promise<unknown> => {
   await NextCors(req, res, {
     methods: ['POST'],
@@ -11,46 +9,15 @@ const minisymposium = async (req: NextApiRequest, res: NextApiResponse): Promise
   });
 
   const { method } = req;
-
   return new Promise((resolve) => {
     if (method === 'GET') {
-      return res.status(200).json({ message: 'Method not allowed. - GET - minisymposium' });
+      console.log('🚀 ~ file: airtable.ts ~ line 29 ~ returnnewPromise ~ resolve', resolve);
+      return res.status(200).json({ message: 'Method not allowed. - GET ' });
     }
 
     if (method === 'POST') {
-      try {
-        const payload = req.body;
-        const minisymposiumLead = createMinisymposium(payload);
-        return res.status(200).json({ status: 'created', minisymposiumLead });
-      } catch (error) {
-        return res.status(422).json({ status: 'not_created', error });
-      }
+      return res.status(200).json({ message: 'Method not allowed. - post ' });
     }
-
-    console.log('🚀 ~ file: index.ts ~ line 17 ~ returnnewPromise ~ resolve', resolve);
-    res.status(400);
-    return res.status(400).json({ message: 'bad_request' });
   });
-
-  // switch (req.method) {
-  //   case 'GET': {
-  //     return res.status(200).json({ message: 'Method not allowed.' });
-  //   }
-  //   case 'POST': {
-  //     try {
-  //       const payload = req.body;
-  //       const minisymposiumLead = await createMinisymposium(payload);
-  //       res.status(200).json({ status: 'created', minisymposiumLead });
-  //     } catch (error) {
-  //       res.status(422).json({ status: 'not_created', error });
-  //     }
-  //     break;
-  //   }
-
-  //   default:
-  //     res.status(400);
-  //     return res.status(400).json({ message: 'bad_request' });
-  // }
 };
-
 export default minisymposium;
