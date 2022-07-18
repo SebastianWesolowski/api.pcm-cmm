56844110770f6449d5cddbb224534060b145d8c7
Sebastian Wesołowski <Sebastian.wesolowski.sw@gmail.com>
2022-07-18 14:44:06 +0200

    asdf

--- src/pages/api/minisymposium/index.ts
+++ src/pages/api/minisymposium/index.ts
@@ -11,25 +11,47 @@ const minisymposium = async (req: NextApiRequest, res: NextApiResponse): Promise
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   });
 
-  switch (req.method) {
-    case 'GET': {
-      return res.status(200).json({ message: 'Method not allowed.' });
+  const { method } = req;
+
+  return new Promise((resolve) => {
+    if (method === 'GET') {
+      return res.status(200).json({ message: 'Method not allowed. - GET - minisymposium' });
     }
-    case 'POST': {
+
+    if (method === 'POST') {
       try {
         const payload = req.body;
-        const minisymposiumLead = await createMinisymposium(payload);
+        const minisymposiumLead = createMinisymposium(payload);
         res.status(200).json({ status: 'created', minisymposiumLead });
       } catch (error) {
         res.status(422).json({ status: 'not_created', error });
       }
-      break;
     }
 
-    default:
-      res.status(400);
-      return res.status(400).json({ message: 'bad_request' });
-  }
+    console.log('🚀 ~ file: index.ts ~ line 17 ~ returnnewPromise ~ resolve', resolve);
+    res.status(400);
+    return res.status(400).json({ message: 'bad_request' });
+  });
+
+  // switch (req.method) {
+  //   case 'GET': {
+  //     return res.status(200).json({ message: 'Method not allowed.' });
+  //   }
+  //   case 'POST': {
+  //     try {
+  //       const payload = req.body;
+  //       const minisymposiumLead = await createMinisymposium(payload);
+  //       res.status(200).json({ status: 'created', minisymposiumLead });
+  //     } catch (error) {
+  //       res.status(422).json({ status: 'not_created', error });
+  //     }
+  //     break;
+  //   }
+
+  //   default:
+  //     res.status(400);
+  //     return res.status(400).json({ message: 'bad_request' });
+  // }
 };
 
 export default minisymposium;
