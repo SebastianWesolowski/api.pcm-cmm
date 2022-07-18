82b4cfcd57ff8399d4f8180e0fd8b04466847b9a
Sebastian Wesołowski <Sebastian.wesolowski.sw@gmail.com>
2022-07-18 13:54:21 +0200

    change

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -1,11 +1,16 @@
-import { NextApiResponse } from 'next';
+import { NextApiRequest, NextApiResponse } from 'next';
+import NextCors from 'nextjs-cors';
 
-const api = async (res: NextApiResponse): Promise<void> => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  await NextCors(req, res, {
+    methods: ['GET'],
+    origin: '*',
+    optionsSuccessStatus: 200,
+  });
+
   try {
     res.json({ message: 'ok' });
   } catch (e) {
     res.status(400).json({ error: (e as Error).message });
   }
 };
-
-export default api;
