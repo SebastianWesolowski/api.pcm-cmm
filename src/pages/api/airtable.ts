3d390347c87ab4994d044abf903beb73a9d3b3c2
Sebastian Wesołowski <Sebastian.wesolowski.sw@gmail.com>
2022-07-19 02:49:30 +0200

    te

--- src/pages/api/airtable.ts
+++ src/pages/api/airtable.ts
@@ -1,32 +1,14 @@
-import Cors from 'cors';
 import { NextApiRequest, NextApiResponse } from 'next';
-// import NextCors from 'nextjs-cors';
-
-const cors = Cors({
-  methods: ['GET', 'POST'],
-});
-
-function runMiddleware(req: any, res: any, fn: any) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result: any) => {
-      if (result instanceof Error) {
-        return reject(result);
-      }
-
-      return resolve(result);
-    });
-  });
-}
+import NextCors from 'nextjs-cors';
 
 import { withSentry } from 'helpers/monitoring/sentry';
 import createMinisymposium from 'services/miniSymposium/createAirtable';
 const handler = async (req: NextApiRequest, res: NextApiResponse): Promise<any> => {
-  await runMiddleware(req, res, cors);
-  // await NextCors(req, res, {
-  //   methods: ['GET', 'POST'],
-  //   origin: '*',
-  //   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-  // });
+  await NextCors(req, res, {
+    methods: ['GET', 'POST'],
+    origin: '*',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
 
   if (req.query.error) {
     throw new Error('Sentry API error test');
@@ -35,7 +17,10 @@ const handler = async (req: NextApiRequest, res: NextApiResponse): Promise<any> 
   const { method } = req;
 
   if (method === 'GET') {
-    res.status(200).json({ status: 'Method GET not allowed - api/minisymposium' });
+    res.json({
+      success: true,
+      message: 'Method GET not allowed - api/minisymposium',
+    });
   }
 
   if (method === 'POST') {
--- src/pages/api/user/register.ts
+++ src/pages/api/user/register.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import NextCors from 'nextjs-cors';
 
 import { withSentry } from 'helpers/monitoring/sentry';
+import createUser from 'services/user/createUser';
 const handler = async (req: NextApiRequest, res: NextApiResponse): Promise<any> => {
   await NextCors(req, res, {
     methods: ['GET', 'POST'],
@@ -9,33 +10,35 @@ const handler = async (req: NextApiRequest, res: NextApiResponse): Promise<any> 
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   });
 
-  if (req.query.error) {
-    throw new Error('Sentry API error test');
-  }
-
   const { method } = req;
 
-  if (method === 'GET') {
-    res.json({
+  if (req.method === 'GET') {
+    res.status(200).json({
       success: true,
       message: 'Method GET not allowed - api/user/register',
     });
-  }
-
-  if (method === 'POST') {
+    res.end();
+  } else if (method === 'POST') {
     try {
       const payload = req.body;
-      // const newUser = await createUser(payload);
-      const newUser = true;
+      const newUser = await createUser(payload);
       res.status(201).json({ status: 'created', newUser, payload });
+      res.end();
     } catch ({ message }) {
       if (message === 'email_taken') {
         const payload = req.body;
         res.status(207).json({ status: 'Conflict on AirTable', email: payload.email });
+        res.end();
       } else {
         res.status(422).json({ status: 'not_created', message });
+        res.end();
       }
     }
+  } else if (req.query.error) {
+    throw new Error('Sentry API error test');
+  } else {
+    res.status(405).json({ status: 'Method not allowed' });
+    res.end();
   }
 };
 
